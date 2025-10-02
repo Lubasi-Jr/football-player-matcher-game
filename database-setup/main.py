f3b72eaf688bfe561e29d39c8a36be3092b796fc
Lubasi Milupi <lubasimilupi@gmail.com>
2025-10-03 00:27:11 +0200

    Manchester United Players Addedd. Data entry algorithm is now ready

--- database-setup/main.py
+++ database-setup/main.py
@@ -5,16 +5,39 @@ import psycopg2
 from pathlib import Path
 load_dotenv()
 
+def getfilepath():
+    base_dir = Path(__file__).parent
+
+    # Construct the path to the markdown file
+    filepath = base_dir / "football-teams" / "machester_united.md"
+    return filepath
+
+def queryExecutor(query,data,cur,conn):
+    cur.execute(query,data)
+    query_result = cur.fetchone()[0]
+    conn.commit()
+    return query_result
+
+def getAllID(cur):
+    query = f"SELECT id FROM player;"
+    cur.execute(query)
+    records = cur.fetchall()
+    id_array = list(map(lambda tp: tp[0],records))
+    all_IDs = set(id_array)
+    return all_IDs
+
+
+
 def main():
     print("Hello from database-setup!")
     connection = psycopg2.connect(dbname="tekk_match",user="postgres",password= os.getenv("PG_PASSWORD"),host="localhost")
     cursor = connection.cursor()
     # Before beginning the extraction process. Obtain all ID and add them to a set. Reduces the number of DB queries
-    
-    base_dir = Path(__file__).parent
+    allPlayerIDs = getAllID(cursor)
+   
 
     # Construct the path to the markdown file
-    filepath = base_dir / "football-teams" / "liverpool_markdown.md"
+    filepath = getfilepath()
     # filepath = "markdown_snippet.md"
     buffer = ""
     with open(filepath,'r',encoding="utf-8") as f:
@@ -30,24 +53,23 @@ def main():
                 player = clean_markdown_line(player_details_array)
 
                 # Check if ID already exists in the database
+                player_id = str(player.createUUID())
 
-                # ID exists- only update the many-to-many table
-                # ID does not exist- create new player and create new row in the player_club_table
-                player_query = f"INSERT INTO player (id,full_name,position,nationality,flag_url,dob) VALUES (%s,%s,%s,%s,%s,%s) RETURNING id;"
-                player_data_to_insert = (str(player.createUUID()),player.fullname,player.position,player.nationality,player.flag_url,player.dob)
-                cursor.execute(player_query,player_data_to_insert)
-                new_player_id = cursor.fetchone()[0]
-                print(f"User inserted successfully with ID: {new_player_id}")
-                connection.commit()
+                if(player_id in allPlayerIDs):
+                    print(f"Player with id: {player_id} has an update to now play for Man United")
+                    played_for_query = f"INSERT INTO played_for (player_id,team_id) VALUES (%s,%s) RETURNING player_id;"
+                    played_for_data_to_insert = (player_id,4)
+                    result = queryExecutor(played_for_query,played_for_data_to_insert,cursor,connection)
+                else:
+                    player_query = f"INSERT INTO player (id,full_name,position,nationality,flag_url,dob) VALUES (%s,%s,%s,%s,%s,%s) RETURNING id;"
+                    player_data_to_insert = (player_id,player.fullname,player.position,player.nationality,player.flag_url,player.dob)
+                    new_player_id = queryExecutor(player_query,player_data_to_insert,cursor,connection)
+                    print(f"New man united player created, id: {new_player_id}")
 
-                played_for_query = f"INSERT INTO played_for (player_id,team_id) VALUES (%s,%s);"
-                played_for_data_to_insert = (new_player_id,1)
-                cursor.execute(played_for_query,played_for_data_to_insert)
-                connection.commit()
-
-                #print("Queries complete, moving on to next player")
-
-            
+                    played_for_query = f"INSERT INTO played_for (player_id,team_id) VALUES (%s,%s) RETURNING player_id;"
+                    played_for_data_to_insert = (new_player_id,4)
+                    result = queryExecutor(played_for_query,played_for_data_to_insert,cursor,connection)
+     
                 # End it with resetting the buffer
                 buffer = ""
             # Line does not yet contain all the info for the player details
