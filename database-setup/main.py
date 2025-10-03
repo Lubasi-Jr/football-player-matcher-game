fe3354f4ba7c4b5385bfdaf2e70641c80a6c1ebf
Lubasi Milupi <lubasimilupi@gmail.com>
2025-10-03 15:05:11 +0200

    Made main function modular and improved the guard clauses

--- database-setup/main.py
+++ database-setup/main.py
@@ -5,11 +5,11 @@ import psycopg2
 from pathlib import Path
 load_dotenv()
 
-def getfilepath():
+def getfilepath(team_name):
     base_dir = Path(__file__).parent
 
     # Construct the path to the markdown file
-    filepath = base_dir / "football-teams" / "machester_united.md"
+    filepath = base_dir / "football-teams" / f"{team_name}.md"
     return filepath
 
 def queryExecutor(query,data,cur,conn):
@@ -37,7 +37,8 @@ def main():
    
 
     # Construct the path to the markdown file
-    filepath = getfilepath()
+    filepath = getfilepath("arsenal")
+    team_id = 3
     # filepath = "markdown_snippet.md"
     buffer = ""
     with open(filepath,'r',encoding="utf-8") as f:
@@ -55,25 +56,19 @@ def main():
                 # Check if ID already exists in the database
                 player_id = str(player.createUUID())
 
-                if(player_id in allPlayerIDs):
-                    print(f"Player with id: {player_id} has an update to now play for Man United")
-                    played_for_query = f"INSERT INTO played_for (player_id,team_id) VALUES (%s,%s) RETURNING player_id;"
-                    played_for_data_to_insert = (player_id,4)
-                    result = queryExecutor(played_for_query,played_for_data_to_insert,cursor,connection)
-                else:
+                if(player_id not in allPlayerIDs):
+                    # Create a new player if this markdown line is not in the list of current players
                     player_query = f"INSERT INTO player (id,full_name,position,nationality,flag_url,dob) VALUES (%s,%s,%s,%s,%s,%s) RETURNING id;"
                     player_data_to_insert = (player_id,player.fullname,player.position,player.nationality,player.flag_url,player.dob)
                     new_player_id = queryExecutor(player_query,player_data_to_insert,cursor,connection)
-                    print(f"New man united player created, id: {new_player_id}")
-
-                    played_for_query = f"INSERT INTO played_for (player_id,team_id) VALUES (%s,%s) RETURNING player_id;"
-                    played_for_data_to_insert = (new_player_id,4)
-                    result = queryExecutor(played_for_query,played_for_data_to_insert,cursor,connection)
-     
+                # Whether new player or not, always update the played for table
+                played_for_query = f"INSERT INTO played_for (player_id,team_id) VALUES (%s,%s) RETURNING player_id;"
+                played_for_data_to_insert = (player_id,team_id)
+                result = queryExecutor(played_for_query,played_for_data_to_insert,cursor,connection)
                 # End it with resetting the buffer
                 buffer = ""
             # Line does not yet contain all the info for the player details
-    
+    print("Player entries complete")
     connection.close()
     cursor.close()
 
