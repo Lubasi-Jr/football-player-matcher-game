f386cde35c1cfea7f84918991df2e9b4d1543e08
Lubasi Milupi <lubasimilupi@gmail.com>
2025-10-28 21:05:23 +0200

    Initialized Spring Backend

---
+++ backend/tekk-match/src/main/java/com/lubasi/tekk_match/TekkMatchApplication.java
@@ -0,0 +1,13 @@
+package com.lubasi.tekk_match;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TekkMatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TekkMatchApplication.class, args);
+	}
+
+}
---
+++ backend/tekk-match/src/test/java/com/lubasi/tekk_match/TekkMatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lubasi.tekk_match;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TekkMatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
